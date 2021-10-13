43cb76dc75fb3426a70b265e71a79907baa5b555
sudeekshaganguli <sudeekshaganguli@gmail.com>
2021-10-13 23:06:57 +0530

    contribution form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
+import Create from './routes/add-contribution-form';
 
 function App() {
   return (
-    <div className="App">
+  <Create/>
+    /*
+     <div className="App">
+      
+      
       <header className="App-header">
+        
+          
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +26,9 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div> */
   );
+
 }
 
 export default App;
